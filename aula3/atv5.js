37f2d92baa9fcd8a618acf166f59046d0c0ef17c
Lucas <lucascantellimedeiros9@gmail.com>
2026-06-14 21:49:28 -0300

    atividades

--- aula3/atv5.js
+++ aula3/atv5.js
@@ -5,7 +5,6 @@ const produtos = [];
 for (let i = 0; i < 3; i++) {
     const nome = readline.question(`Digite o nome do produto ${i + 1}: `);
     
-    // o que vem do readline é sempre TEXTO, por isso convertemos com Number()
     const preco = Number(readline.question(`Digite o preço do produto ${i + 1}: `));
 
     produtos.push({ nome: nome, preco: preco });
