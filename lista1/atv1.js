20e7ff1c5b3f2d21471cb4667bed21341101f512
unknown <lucas>
2026-06-21 17:12:56 -0300

    atividades lista01

---
+++ lista1/atv1.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync');
+
+let c = readline.questionFloat("Digite a temperatura a ser converida: ");
+
+let f = c * 1.8 + 32;
+
+console.log(`A temperaura em fahrenheit é ${f}`);
---
+++ lista1/atv9.js
@@ -0,0 +1,7 @@
+//9 – Mínimo. Escreva uma função min(a, b) que receba dois números e retorne o menor deles. Não use Math.min. Escreva a versão tradicional e depois reescreva como Arrow Function.]
+
+function min(a,b) {
+    return (a > b) ? a : b
+}
+
+const minArrow =
